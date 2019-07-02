2529a0378eec83c3795c32235210466cba1ca261
vardaro <anthonyavardaro@yahoo.com>
2019-07-01 22:24:07 -0400

    hw03

---
+++ aav2383-hw03/cmake-build-debug/CMakeFiles/aav2383_hw03.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aav2383_hw03.dir/hw03.c.o"
+  "aav2383_hw03.pdb"
+  "aav2383_hw03"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/aav2383_hw03.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
